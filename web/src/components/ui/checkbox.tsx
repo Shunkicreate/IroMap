1dd351dd21c46a8a62485680ef1d8f5db368ce77
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2026-03-25 23:57:17 +0900

    Use monochrome checkbox styling

--- web/src/components/ui/checkbox.tsx
+++ web/src/components/ui/checkbox.tsx
@@ -11,11 +11,11 @@ function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxP
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer inline-flex !size-4 shrink-0 items-center justify-center rounded-[0.3125rem] !border border-border !p-0 !shadow-none transition-[background-color,border-color,box-shadow,transform] outline-none",
+        "peer inline-flex !size-4 shrink-0 items-center justify-center rounded-[0.3125rem] !border !border-border !p-0 !shadow-none transition-[background-color,border-color,color,box-shadow,transform] outline-none",
         "!bg-background !text-transparent",
         "focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
         "disabled:cursor-not-allowed disabled:opacity-50",
-        "data-[state=checked]:!border-primary data-[state=checked]:!bg-primary data-[state=checked]:!text-primary-foreground",
+        "data-[state=checked]:!border-foreground data-[state=checked]:!bg-foreground data-[state=checked]:!text-background",
         className
       )}
       {...props}
